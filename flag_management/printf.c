2392ab166d84e42aa5971581872f9c25b56833dc
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-03-07 15:02:00 +0900

    [WIP/Update] Handle %x with min_field_width, precision and some flags.
    Almost all the case is handled current implementation, but for one case.
    When 0 is given to the precision, the original printf is print nothing,
    but mine is print single character, fix it before moveon to next task.
    
    Also, I suppose to create print logic full scratch. Due to complexity of
    printing padding character etc.

--- flag_management/printf.c
+++ flag_management/printf.c
@@ -33,9 +33,74 @@ void	put_di(int nbr, t_finfo *info)
 	free(s_nbr);
 }
 
+size_t	get_padding_len_x(size_t len, unsigned int nbr, t_finfo *info)
+{
+	size_t	padding_len;
+
+	padding_len = 0;
+	if (info->alter_flag && nbr != 0)
+		len += 2;
+	if (info->min_field_width >= 0 && info->min_field_width > len)
+		padding_len = info->min_field_width - len;
+	return (padding_len);
+}
+
+void	put_number_hex_l(char *s_nbr, size_t padding_len, t_finfo *info)
+{
+	if (info->minus_flag)
+	{
+		if (info->alter_flag && atoi(s_nbr) != 0)
+			ft_putstr("0x");
+		ft_putstr(s_nbr);
+		put_padding(padding_len, info);
+	}
+	else
+	{
+		put_padding(padding_len, info);
+		if (info->alter_flag && atoi(s_nbr) != 0)
+			ft_putstr("0x");
+		ft_putstr(s_nbr);
+	}
+}
+
+void	put_padded_number_hex_l(
+		char *s_nbr, size_t padding_len, size_t padding_len_num, t_finfo *info)
+{
+	if (info->minus_flag)
+	{
+		if (info->alter_flag && atoi(s_nbr) != 0)
+			ft_putstr("0x");
+		while (padding_len_num--)
+			ft_putchar('0');
+		ft_putstr(s_nbr);
+		put_padding(padding_len, info);
+	}
+	else
+	{
+		put_padding(padding_len, info);
+		if (info->alter_flag && atoi(s_nbr) != 0)
+			ft_putstr("0x");
+		while (padding_len_num--)
+			ft_putchar('0');
+		ft_putstr(s_nbr);
+	}
+}
+
+size_t	n_len_x(char *s_nbr, t_finfo *info)
+{
+	size_t	num_len;
+
+	num_len = ft_strlen(s_nbr);
+	if (info->alter_flag && atoi(s_nbr) != 0)
+		num_len += 2;
+	return (num_len);
+}
+
 void	put_x_lower(unsigned int nbr, t_finfo *info)
 {
 	char	*s_nbr;
+	size_t	padding_len;
+	size_t	padding_len_num;
 
 	s_nbr = ulonglong_toa_base(nbr, BASE_16L);
 	if (s_nbr == NULL)
@@ -43,9 +108,19 @@ void	put_x_lower(unsigned int nbr, t_finfo *info)
 		info->is_error = true;
 		return ;
 	}
-	if (info->alter_flag && nbr != 0)
-		ft_putstr("0x");
-	ft_putstr(s_nbr);
+	padding_len = get_padding_len_x(get_print_len_num(s_nbr, info), nbr, info);
+	padding_len_num = get_padding_len_num(ft_strlen(s_nbr), info);
+	if (is_overflow(padding_len + padding_len_num + n_len_x(s_nbr, info), info))
+	{
+		info->total_len = ERROR;
+		return ;
+	}
+	else
+		info->total_len += padding_len + padding_len_num + n_len_x(s_nbr, info);
+	if (padding_len_num > 0)
+		put_padded_number_hex_l(s_nbr, padding_len, padding_len_num, info);
+	else
+		put_number_hex_l(s_nbr, padding_len, info);
 	free(s_nbr);
 }
 
@@ -136,9 +211,76 @@ int	main(void)
 	int	ret1;
 	int	ret2;
 
-	ret1 = printf("[%-013.10p]\n", main);
-	ret2 = ft_printf("[%-013.10p]\n", main);
-	printf("\nret1: %d, ret2: %d\n", ret1, ret2);
+	ret1 = printf("[%x]\n", 32);
+	ret2 = ft_printf("[%x]\n", 32);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%.3x]\n", 32);
+	ret2 = ft_printf("[%.3x]\n", 32);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%4.3x]\n", 32);
+	ret2 = ft_printf("[%4.3x]\n", 32);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%-4.3x]\n", 32);
+	ret2 = ft_printf("[%-4.3x]\n", 32);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	puts("aletrnate_flag");
+	ret1 = printf("[%#x]\n", 32);
+	ret2 = ft_printf("[%#x]\n", 32);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%#.3x]\n", 32);
+	ret2 = ft_printf("[%#.3x]\n", 32);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%#6.3x]\n", 32);
+	ret2 = ft_printf("[%#6.3x]\n", 32);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%-#6.3x]\n", 32);
+	ret2 = ft_printf("[%-#6.3x]\n", 32);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	puts("aletrnate_flag with value 0");
+	ret1 = printf("[%#x]\n", 0);
+	ret2 = ft_printf("[%#x]\n", 0);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%#.3x]\n", 0);
+	ret2 = ft_printf("[%#.3x]\n", 0);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%#6.3x]\n", 0);
+	ret2 = ft_printf("[%#6.3x]\n", 0);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%-#6.3x]\n", 0);
+	ret2 = ft_printf("[%-#6.3x]\n", 0);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	puts("precision 0");
+	ret1 = printf("[%.x]\n", 0);
+	ret2 = ft_printf("[%.x]\n", 0);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%#.0x]\n", 0);
+	ret2 = ft_printf("[%#.0x]\n", 0);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%#6.0x]\n", 0);
+	ret2 = ft_printf("[%#6.0x]\n", 0);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	ret1 = printf("[%-#6.0x]\n", 0);
+	ret2 = ft_printf("[%-#6.0x]\n", 0);
+	printf("ret1: %d, ret2: %d\n\n", ret1, ret2);
+
+	/* ret1 = printf("[%-013.10p]\n", main); */
+	/* ret2 = ft_printf("[%-013.10p]\n", main); */
+	/* printf("\nret1: %d, ret2: %d\n", ret1, ret2); */
 
 	/* ret1 = printf("[%12p]\n", main); */
 	/* ret2 = ft_printf("[%12p]\n", main); */

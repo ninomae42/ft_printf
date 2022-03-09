bd9ac5e53af2f9ac2bcde69b5df52cfc3dbc8b69
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-03-09 22:52:55 +0900

    [Fix] Fix submodule issue

---
+++ libft/ft_strlen.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tashimiz <tashimiz@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 16:59:48 by tashimiz          #+#    #+#             */
+/*   Updated: 2022/01/27 16:59:49 by tashimiz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+// find length of string
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	if (s == NULL)
+		return (0);
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
